0fe1f15fb7ae8a13aed1a19302f562279bd5057f
Enes Emre Yılmaz <enesemreyl@gmail.com>
2023-11-04 21:51:18 +0300

    screen bug fixed

--- Assets/Scripts/BombLeftRight.cs
+++ Assets/Scripts/BombLeftRight.cs
@@ -29,37 +29,38 @@ public class BombLeftRight: MonoBehaviour
          {
              Vector3 move = new Vector3(0, bombSpeed, 0);
              transform.Translate(move);
+             if (Input.touchCount > 0)
+             {
+                 touch = Input.GetTouch(0);
+                 {
+                     if (touch.phase == TouchPhase.Moved)
+                     {
+                         transform.position = new Vector3(transform.position.x + touch.deltaPosition.x * -speed,
+                             transform.position.y, transform.position.z);
+
+                         if (transform.position.x <= maxDistanceRight)
+                         {
+                             transform.position = new Vector3(maxDistanceRight, transform.position.y, transform.position.z);
+                         }
+                         else if (transform.position.x >= maxDistanceLeft)
+                         {
+                             transform.position = new Vector3(maxDistanceLeft, transform.position.y, transform.position.z);
+                         }
+                     }
+                 }// private void Update()
+                 // {
+                 //     if (rotateComplete)
+                 //     {
+                 //         Vector3 move = new Vector3(transform.position.x, -bombSpeed * Time.deltaTime,
+                 //             transform.position.z);
+                 //        transform.Translate(move);
+                 //     }
+                 //     
+                 // }
+             }
          }
          
-         if (Input.touchCount > 0)
-         {
-             touch = Input.GetTouch(0);
-             {
-                 if (touch.phase == TouchPhase.Moved)
-                 {
-                     transform.position = new Vector3(transform.position.x + touch.deltaPosition.x * -speed,
-                         transform.position.y, transform.position.z);
-
-                     if (transform.position.x <= maxDistanceRight)
-                     {
-                         transform.position = new Vector3(maxDistanceRight, transform.position.y, transform.position.z);
-                     }
-                     else if (transform.position.x >= maxDistanceLeft)
-                     {
-                         transform.position = new Vector3(maxDistanceLeft, transform.position.y, transform.position.z);
-                     }
-                 }
-             }// private void Update()
-             // {
-             //     if (rotateComplete)
-             //     {
-             //         Vector3 move = new Vector3(transform.position.x, -bombSpeed * Time.deltaTime,
-             //             transform.position.z);
-             //        transform.Translate(move);
-             //     }
-             //     
-             // }
-         }
+         
      }
 }
 
