7b1fba30bc89b63b0bc402d2dfea5427bc821e10
suleyman-aris <suleymanaris26@gmail.com>
2023-11-11 01:29:54 +0300

    kiloton ui visitable

--- Assets/Scripts/Button/KTon Text/KTonText.cs
+++ Assets/Scripts/Button/KTon Text/KTonText.cs
@@ -3,13 +3,19 @@ using UnityEngine;
 
 public class KTonText : MonoBehaviour
 {
+    public FollwChange follwChange;
     private void Start()
     {
         KiloTonCalculate.kiloTonCalculate.kTon += KTonText_kTon;
+        follwChange.changeCamera += SetFalse;
     }
 
     private void KTonText_kTon(int kTon)
     {
         GetComponent<TextMeshProUGUI>().text = kTon.ToString();
+    }
+    private void SetFalse()
+    {
+        transform.parent.gameObject.SetActive(false);
     }
 }
