0363deb57c6cb9ba7be99c667ad6b9a32a8b8e8e
Enes Emre Yılmaz <129618940+enesemreyilmaz@users.noreply.github.com>
2023-11-11 00:44:06 +0300

    recoil buglı

--- Assets/Scripts/Bomp/Mini Bomp/MiniBompSpawn.cs
+++ Assets/Scripts/Bomp/Mini Bomp/MiniBompSpawn.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class MiniBompSpawn : MonoBehaviour      // kodun new bomps�n head�na at yoksa i�aretli sat�r �al��maz
 {
@@ -8,8 +9,15 @@ public class MiniBompSpawn : MonoBehaviour      // kodun new bomps�n head�na
     private Drop dropCs;
     public Drop dropCs2;
     private bool isSpawn;
+    
+    //açılar
+    private float x1 = -180;
+    private float x = -190f;
+    private float y = 180f;
+    private float z = 0f;
     private void Start()
-    {        
+    {
+        
         follwChange.changeCamera += SpawnStop;        
         if (dropCs2 != null)
         {
@@ -31,6 +39,7 @@ public class MiniBompSpawn : MonoBehaviour      // kodun new bomps�n head�na
         {
             yield return new WaitForSeconds(MiniBompManager.miniBompManager.spawnSpeed);
             MiniBompList.miniBompList.miniBomp[0].SetActive(true);
+            Recoil();
             MiniBompList.miniBompList.miniBomp[0].transform.position = transform.position;
             MiniBompList.miniBompList.miniBomp.RemoveAt(0);
             
@@ -46,5 +55,17 @@ public class MiniBompSpawn : MonoBehaviour      // kodun new bomps�n head�na
     private void SpawnStop()
     {
         isSpawn = false;
+    }
+
+    private void Recoil()
+    
+    {
+        Quaternion firstRotation = Quaternion.Euler(x1, y, z);
+        Quaternion recoil = Quaternion.Euler(x, y, z);
+
+        transform.parent.DORotateQuaternion(recoil, 0.2f).SetEase(Ease.Linear).OnComplete(() =>
+        {
+           transform.parent.DORotateQuaternion(firstRotation, 0.2f).SetEase(Ease.Linear);
+        });
     }
 }
