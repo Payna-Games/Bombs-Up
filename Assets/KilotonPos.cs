e0ba13f35e516723e833d1e9b53a2193fc7f11a6
Enes Emre Yılmaz <enesemreyl@gmail.com>
2023-11-16 02:25:58 +0300

    kiloton canvas fix

--- Assets/KilotonPos.cs
+++ Assets/KilotonPos.cs
@@ -10,18 +10,21 @@ public class KilotonPos : MonoBehaviour
 {
     // private Image kilotonImage;
     private RectTransform kilotonTextRectTransform;
+    
 
     private void Start()
     {
-        //kilotonImage = GetComponent<Image>();
+        
         kilotonTextRectTransform = GetComponent<RectTransform>();
-       
+        
+
     }
 
     public IEnumerator TextRotater()
     {
+        transform.GetChild(0).gameObject.SetActive(false);
         yield return new WaitForSeconds(3f);
-
+        transform.GetChild(0).gameObject.SetActive(true);
         kilotonTextRectTransform.anchoredPosition3D= new Vector3(-0.58f, -5f,0.27f); 
         kilotonTextRectTransform.localRotation=Quaternion.Euler(-15f,0,0);
         //  kilotonTextRectTransform.localPosition = new Vector3(-0.51f, -5.37f, 0f);
