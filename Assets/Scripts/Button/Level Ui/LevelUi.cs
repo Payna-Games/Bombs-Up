5a3e5b22a57f4dbca7e2f25c4c7f73b1ec5fec72
suleyman-aris <suleymanaris26@gmail.com>
2023-11-16 14:48:36 +0300

    save scene

--- Assets/Scripts/Button/Level Ui/LevelUi.cs
+++ Assets/Scripts/Button/Level Ui/LevelUi.cs
@@ -10,11 +10,21 @@ public class LevelUi : MonoBehaviour
     void Start()
     {
         if (SceneManager.GetActiveScene().name.Contains("Tutorial"))
+        {
             GetComponent<TextMeshProUGUI>().text = "Tutorial";
+            if (!PlayerPrefs.HasKey(transform.name))
+            {
+                PlayerPrefs.SetInt(transform.name, SceneManager.GetActiveScene().buildIndex);
+            }
+            else
+                SceneManager.LoadScene(PlayerPrefs.GetInt(transform.name));
+        }
+
         else
         {
             string[] parts = SceneManager.GetActiveScene().name.Split('-');
             GetComponent<TextMeshProUGUI>().text = "Lvl " + parts[1];
+            PlayerPrefs.SetInt(transform.name, SceneManager.GetActiveScene().buildIndex);
         }
     }
 
