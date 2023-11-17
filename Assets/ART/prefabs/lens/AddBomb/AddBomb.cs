b9474fb7006e9d866ef63cb270ce2d79b2cd31ba
suleyman-aris <suleymanaris26@gmail.com>
2023-11-17 22:09:44 +0300

    addbomb lens calculate kiloton

--- Assets/ART/prefabs/lens/AddBomb/AddBomb.cs
+++ Assets/ART/prefabs/lens/AddBomb/AddBomb.cs
@@ -12,77 +12,77 @@ public class AddBomb : MonoBehaviour
     [SerializeField] private ParticleSystem waterParticle;
     [SerializeField] private Transform particleTransform;
 
-    [SerializeField]  private Image rocketImage;
+    [SerializeField] private Image rocketImage;
     [SerializeField] private float fillAmountValue;
     [SerializeField] private float initialScale = 1f;
     [SerializeField] private float targetScale = 1.5f;
     [SerializeField] private float duration = 1f;
     private bool savedLens = false;
     public bool addBombLens;
-     private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("MiniBomb"))
         {
 
             rocketImage.fillAmount += fillAmountValue;
-            
-            
+
+
             CreateParticle.Create(transform.position);
             savedLens = true;
             Destroy(other.gameObject);
-            
+
         }
 
-        if ((other.CompareTag("Bomb")&& rocketImage.fillAmount ==1 ))
+        if ((other.CompareTag("Bomb") && rocketImage.fillAmount == 1))
         {
 
             CreateParticle.ParticleTransform.gameObject.SetActive(false);
             for (int i = 0; i < other.transform.GetChild(4).childCount; i++)
+            {
+                if (!other.transform.GetChild(4).GetChild(i).gameObject.activeSelf)
                 {
-                    if (!other.transform.GetChild(4).GetChild(i).gameObject.activeSelf)
-                    {
-                        GameObject clone = other.transform.GetChild(4).GetChild(i).gameObject;
-                        clone.SetActive(true);
-                        other.transform.GetChild(4).GetChild(i).DOScale(Vector3.one*targetScale,0.2f).SetEase(Ease.Linear).OnComplete(() =>
-                        {
+                    GameObject clone = other.transform.GetChild(4).GetChild(i).gameObject;
+                    clone.SetActive(true);
+                    other.transform.GetChild(4).GetChild(i).DOScale(Vector3.one * targetScale, 0.2f).SetEase(Ease.Linear).OnComplete(() =>
+                       {
 
-                            clone.transform.DOScale(Vector3.one* initialScale, duration)
-                                .SetEase(Ease.InBounce);
+                           clone.transform.DOScale(Vector3.one * initialScale, duration)
+                               .SetEase(Ease.InBounce);
 
-                        });
-                       // clone.transform.GetChild(0).GetComponent<KiloTonCalculate>().Calculate();
-                       addBombLens = true;
-                        break;
-                    }
+                       });
+                    // clone.transform.GetChild(0).GetComponent<KiloTonCalculate>().Calculate();
+                    addBombLens = true;
+                    break;
                 }
+            }
 
-           
+            other.gameObject.GetComponent<KiloTonCalculate>().Calculate();
             gameObject.SetActive(false);
         }
-        else if((other.CompareTag("Bomb")&& fillAmountValue !=1 ))
+        else if ((other.CompareTag("Bomb") && fillAmountValue != 1))
         {
-                
+
             gameObject.SetActive(false);
-            
+
         }
     }
-     
-     private void Update()
-     {
-        
-         if (savedLens )
-         {
-             CreateParticle.GetLensPosition(transform.position);
-             StartCoroutine(SavedLens());
-         }
-       
-     }
 
-     private IEnumerator SavedLens()
-     {
-         yield return new WaitForSeconds(2.5f);
-         savedLens = false;
-    
-     }
-    
+    private void Update()
+    {
+
+        if (savedLens)
+        {
+            CreateParticle.GetLensPosition(transform.position);
+            StartCoroutine(SavedLens());
+        }
+
+    }
+
+    private IEnumerator SavedLens()
+    {
+        yield return new WaitForSeconds(2.5f);
+        savedLens = false;
+
+    }
+
 }
