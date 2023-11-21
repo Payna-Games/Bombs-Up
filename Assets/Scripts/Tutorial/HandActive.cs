0a625ada01e3b91d491f74ab7cad3b053da77ecb
suleyman-aris <suleymanaris26@gmail.com>
2023-11-22 02:25:08 +0300

    Update HandActive.cs

--- Assets/Scripts/Tutorial/HandActive.cs
+++ Assets/Scripts/Tutorial/HandActive.cs
@@ -13,7 +13,7 @@ public class HandActive : MonoBehaviour
         if (SlotAddButton.slotAddButton.transform.GetComponent<EnoughMoney>().clickCount >=3 && bomp.GetChild(1).GetComponent<ObjectLevel>().objectLevel > 0 && ClickCount.clickCount.goClickCount >= 1)
         {
             ClickCount.clickCount.GetComponent<Button>().interactable = true;
-
+            incomeButton.GetComponent<EnoughMoney>().CanInteract = true;
             masks.gameObject.SetActive(false);
             gameObject.SetActive(false);
         }
