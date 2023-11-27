49b1b9d29b253fe7a108e1635ec045a16ff7ed62
Enes Emre Yılmaz <enesemreyl@gmail.com>
2023-11-27 23:12:54 +0300

    fix2

--- Assets/Scripts/LensWaitTimee.cs
+++ Assets/Scripts/LensWaitTimee.cs
@@ -18,7 +18,7 @@ public class LensWaitTime : MonoBehaviour
 
     public  IEnumerator LensActive()
     {
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(1.5f);
         lensActive = false;
             
     
