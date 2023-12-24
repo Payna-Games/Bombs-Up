7c60cc5d9e849558360730d09ddd716fc6aed865
suleyman-aris <suleymanaris26@gmail.com>
2023-12-24 21:15:53 +0300

    sell and 3x3 grid preafeb edit

--- Assets/Scripts/Button/SellImage.cs
+++ Assets/Scripts/Button/SellImage.cs
@@ -1,15 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class SellImage : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
+    Vector3 originalScale;
+    private void Start()
     {
-        Sell.sell.mouseUp = true;
+        originalScale = transform.localScale;
+    }
+    private void OnTriggerEnter(Collider other)
+    {Sell.sell.mouseUp = true;
+        transform.DOScale(originalScale * 0.85f, 1.0f);
+        
     }
     private void OnTriggerExit(Collider other)
-    {
-        Sell.sell.mouseUp = false;
+    {Sell.sell.mouseUp = false;
+        transform.DOScale(originalScale, 1.0f);
+        
     }
 }
--- Assets/Scripts/Manager/Sell/Sell.cs
+++ Assets/Scripts/Manager/Sell/Sell.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 using UnityEngine.UI;
-using DG.Tweening;
 using System.Collections;
 public class Sell : MonoBehaviour
 {
@@ -8,20 +7,12 @@ public class Sell : MonoBehaviour
     public bool IsOnImage;
 
     public static Sell sell;
-    private Vector3 normalSize;
     public float scaleFactor = 1.3f;
     public float duration = 1.0f;
-    Vector3 originalScale;
-    private RectTransform rectTransform;
-    Vector2 targetSize;
     public bool mouseUp;
     private void Awake()
     {
         sell = sell == null ? this : sell;
-        originalScale = transform.localScale;
-        //rectTransform = GetComponent<RectTransform>();
-        //normalSize = transform.localScale;
-        //targetSize = rectTransform.sizeDelta * scaleFactor;
     }
 
     public void OnPointExit()
@@ -30,7 +21,6 @@ public class Sell : MonoBehaviour
         {
             IsOnImage = false;
             ObjectList.objectList.DragObjectNow = null;
-            transform.DOScale(originalScale, 1.0f);
             MoneyManager.moneyManager.buttonClicked = true;
             MoneyManager.moneyManager.InreaseTotalMoney((float)(addButton.GetComponent<EnoughMoney>().enough * 0.75f));
         }
@@ -41,7 +31,6 @@ public class Sell : MonoBehaviour
         if (ObjectList.objectList.DragObjectNow != null)
         {
             IsOnImage = true;
-            transform.DOScale(originalScale * 0.7f, 1.0f);
             if (VibratorManager.vibratorManager.mainVibrator)
             {
                 Vibrator.Vibrate();
