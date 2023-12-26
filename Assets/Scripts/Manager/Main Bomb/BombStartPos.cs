9364d1d83e53f24f78899a3f95f594248e54688a
suleyman-aris <suleymanaris26@gmail.com>
2023-12-26 18:28:16 +0300

    tutorial level edit

--- Assets/Scripts/Manager/Main Bomb/BombStartPos.cs
+++ Assets/Scripts/Manager/Main Bomb/BombStartPos.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BombStartPos : MonoBehaviour
 {
@@ -8,5 +9,9 @@ public class BombStartPos : MonoBehaviour
     void Start()
     {
         transform.position = new Vector3(0.7f, 6.4f, 23.4f);
+        if (SceneManager.GetActiveScene().name == "Level-1-Tutorial")
+        {
+            transform.position = new Vector3(2.7f, 6.4f, 23.4f);
+        }
     }
 }
