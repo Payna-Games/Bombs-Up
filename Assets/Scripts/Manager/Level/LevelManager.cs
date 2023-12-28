4878a30ac24d28efefa00a8ca68fb99ec00dfb44
ysflkrbg <101269391+ysflkrbg@users.noreply.github.com>
2023-12-28 17:55:33 +0300

    3

--- Assets/Scripts/Manager/Level/LevelManager.cs
+++ Assets/Scripts/Manager/Level/LevelManager.cs
@@ -8,6 +8,7 @@ public class LevelManager : MonoBehaviour
         if (!PlayerPrefs.HasKey(transform.name))
         {
             PlayerPrefs.SetInt(transform.name, 1);
+            PlayerPrefs.SetInt("Vibrator", 0);
             SceneManager.LoadScene(1);
         }
         else
